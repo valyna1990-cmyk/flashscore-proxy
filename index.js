4bad1bfce1088eb28ab3ac1de2d6d774d91f3ede
valyna1990-cmyk <valyna1990@gmail.com>
2025-11-21 20:50:53 +0300

    Update proxy settings and change server port

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
@@ -10,21 +9,28 @@ app.get('/proxy', async (req, res) => {
     try {
         const targetPath = req.query.url;
         if (!targetPath) return res.status(400).send("Missing url parameter");
+
         const targetUrl = "https://d.flashscore.com" + targetPath;
 
         const response = await axios.get(targetUrl, {
             headers: {
-                'User-Agent':
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-                'Accept': '*/*'
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Referer": "https://www.flashscore.com/",
+                "Origin": "https://www.flashscore.com",
+                "X-Fsign": "SW9D1eZo",
             },
-            timeout: 15000
+            responseType: "text",
+            decompress: true,
+            transformResponse: r => r
         });
 
-        res.set('Content-Type', 'text/plain; charset=utf-8');
+        res.set("Content-Type", "text/plain; charset=utf-8");
         res.send(response.data);
     } catch (err) {
-        res.status(500).send("Proxy error: " + err.toString());
+        res.status(500).send("Proxy error: " + err.message);
     }
 });
 
@@ -32,5 +38,5 @@ app.get('/', (req, res) => {
     res.send("FlashScore Proxy is running");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => console.log("Proxy running on port " + port));
